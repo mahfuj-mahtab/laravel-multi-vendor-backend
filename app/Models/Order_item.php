625569922b44c9544ca7ffff8d29b9dd9872a533
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-02-05 00:07:58 +0600

    model created

--- app/Models/Order_item.php
+++ app/Models/Order_item.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order_item extends Model
 {
-    //
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'unit_price'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
